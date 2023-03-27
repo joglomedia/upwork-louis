dbca51abcbdfc8375ec4058409aabbbde2197863
Edi Septriyanto <me@masedi.net>
2023-03-27 23:03:45 +0700

    Update WP config

--- data/wordpress/wp-config.php
+++ data/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'dbuser' );
 define( 'DB_PASSWORD', 'passwd12345' );
 
 /** Database hostname */
-define( 'DB_HOST', 'db' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
