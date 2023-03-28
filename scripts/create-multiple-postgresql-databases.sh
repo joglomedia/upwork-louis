327d244bbe65a58b910b24ad2245af56752345c7
Edi Septriyanto <me@masedi.net>
2023-03-29 00:41:48 +0700

    Update Postgres multiple db

--- scripts/create-multiple-postgresql-databases.sh
+++ scripts/create-multiple-postgresql-databases.sh
@@ -5,14 +5,7 @@ function create_user_and_database() {
 	local role=$(echo $1 | tr ':' ' ' | awk  '{print $2}')
 	echo "Creating user '${role}' and database '${database}'..."
 	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-PGSQL
-		DO $do$
-		BEGIN
-			IF EXISTS ( SELECT FROM pg_catalog.pg_roles WHERE rolname = '${role}' ) THEN
-				RAISE NOTICE 'Role "${role}" already exists. Skipping.';
-			ELSE
-				CREATE ROLE ${role} LOGIN PASSWORD '${POSTGRES_PASSWORD}';
-			END IF;
-		END $do$;
+		CREATE ROLE ${role} LOGIN PASSWORD '${POSTGRES_PASSWORD}';
 		CREATE DATABASE '${database}';
 		GRANT ALL PRIVILEGES ON DATABASE '${database}' TO '${role}';
 PGSQL
