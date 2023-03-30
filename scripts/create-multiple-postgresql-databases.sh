95dc59b8a0f2ce27377d9457954bc5183639e3b3
Edi Septriyanto <me@masedi.net>
2023-03-30 22:03:55 +0700

    Update compose config

--- scripts/create-multiple-postgresql-databases.sh
+++ scripts/create-multiple-postgresql-databases.sh
@@ -10,7 +10,7 @@ function create_user_and_database() {
 	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-PGSQL
 		CREATE ROLE ${role} LOGIN PASSWORD '${POSTGRES_PASSWORD}';
 		CREATE DATABASE ${database};
-		GRANT ALL PRIVILEGES ON DATABASE '${database}' TO '${role}';
+		GRANT ALL PRIVILEGES ON DATABASE ${database} TO ${role};
 PGSQL
 }
 
