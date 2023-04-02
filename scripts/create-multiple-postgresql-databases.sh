ab565f76cae29615315b1653f9d1bd5e4663f401
Edi Septriyanto <me@masedi.net>
2023-04-02 15:56:58 +0700

    Update postgres config

--- scripts/create-multiple-postgresql-databases.sh
+++ scripts/create-multiple-postgresql-databases.sh
@@ -2,9 +2,9 @@
 
 function create_user_and_database() {
 	local MDB_DB_NAME=$(echo $1 | tr ':' ' ' | awk  '{print $1}')
-	#local MDB_ROLE=$(echo $1 | tr ':' ' ' | awk  '{print $2}')
-	local MDB_ROLE="${POSTGRES_USER}"
-	#local MDB_PASSWORD=$(echo $1 | tr ':' ' ' | awk  '{print $3}')
+	local MDB_ROLE=$(echo $1 | tr ':' ' ' | awk  '{print $2}')
+	#local MDB_ROLE="${POSTGRES_USER}"
+	local MDB_PASSWORD=$(echo $1 | tr ':' ' ' | awk  '{print $3}')
 
 	if [[ "${MDB_PASSWORD}x" == "x" ]]; then
 		MDB_PASSWORD="${POSTGRES_PASSWORD}"
@@ -14,6 +14,7 @@ function create_user_and_database() {
 
 	# CREATE ROLE ${MDB_ROLE} LOGIN PASSWORD '${MDB_PASSWORD}';
 	psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" <<-PGSQL
+		CREATE ROLE ${MDB_ROLE} LOGIN PASSWORD '${MDB_PASSWORD}';
 		CREATE DATABASE ${MDB_DB_NAME};
 		GRANT ALL PRIVILEGES ON DATABASE ${MDB_DB_NAME} TO ${MDB_ROLE};
 PGSQL
